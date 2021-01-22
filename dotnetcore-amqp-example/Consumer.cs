131b9c836c279ff72052623904339a80b5a570eb
Johan Nordlund <johan.n@84codes.com>
2021-01-22 11:30:59 +0100

    Initial commit, dotnetcore-amqp-example

---
+++ dotnetcore-amqp-example/Consumer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace dotnetcore_amqp_example
+{
+    public class Consumer
+    {
+        private ManualResetEvent _resetEvent = null;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public Consumer(string url, ManualResetEvent resetEvent)
+        {
+            _resetEvent = resetEvent;
+
+            // create a connection and open a channel, dispose them when done
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(url)
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+        }
+
+        public void ConsumeQueue()
+        {
+            // ensure that the queue exists before we access it
+            var queueName = "queue1";
+            bool durable = false;
+            bool exclusive = false;
+            bool autoDelete = true;
+
+            _channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
+
+            var consumer = new EventingBasicConsumer(_channel);
+
+            // add the message receive event
+            consumer.Received += (model, deliveryEventArgs) =>
+            {
+                var body = deliveryEventArgs.Body.ToArray();
+                // convert the message back from byte[] to a string
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("** Received message: {0} by Consumer thread **", message);
+                // ack the message, ie. confirm that we have processed it
+                // otherwise it will be requeued a bit later
+                _channel.BasicAck(deliveryEventArgs.DeliveryTag, false);
+            };
+
+            // start consuming
+            _ = _channel.BasicConsume(consumer, queueName);
+
+            // Wait for the reset event and clean up when it triggers
+            _resetEvent.WaitOne();
+            Console.WriteLine("CancelEvent recieved, shutting down Consumer");
+            _channel?.Close();
+            _channel = null;
+            _connection?.Close();
+            _connection = null;
+        }
+    }
+}
---
+++ dotnetcore-amqp-example/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace dotnetcore_amqp_example
+{
+    class Program
+    {
+        // The url of the AMQP server, should look like amqps://[username]:[password]@[instance]/[vhost]
+        static readonly string _url = "amqps://[username]:[password]@[instance]/[vhost]";
+        static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
+        public static int Main(string[] args)
+        {
+            Console.CancelKeyPress += (sender, eArgs) => {
+                // set the quit event so that the Consumer will recieve it and quit gracefully
+                _quitEvent.Set();
+                Console.WriteLine("CancelEvent recieved, shutting down...");
+                // sleep 1 second to give Consumer time to clean up
+                Thread.Sleep(1000);
+            };
+
+            // setup worker thread
+            var consumer = new Consumer(_url, _quitEvent);
+            var consumerThread = new Thread(consumer.ConsumeQueue) { IsBackground = true };
+            consumerThread.Start();
+
+            // create a connection and open a channel, dispose them when done
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri(_url)
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            // ensure that the queue exists before we publish to it
+            var queueName = "queue1";
+            bool durable = false;
+            bool exclusive = false;
+            bool autoDelete = true;
+
+            channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
+
+            while (true)
+            {
+                Console.WriteLine("Enter a message and publish with pressing the return key (exit with ctrl-c)");
+                var message = Console.ReadLine();
+                // the data put on the queue must be a byte array
+                var data = Encoding.UTF8.GetBytes(message);
+                // publish to the "default exchange", with the queue name as the routing key
+                var exchangeName = "";
+                var routingKey = queueName;
+                channel.BasicPublish(exchangeName, routingKey, null, data);
+                Console.WriteLine("Published message {0}", message);
+            }
+        }
+    }
+}
